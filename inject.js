fc5f579fe69b082653fd46bd0a2741358e1c2e86
Ivanmatthew <76107136+Ivanmatthew@users.noreply.github.com>
2022-10-18 01:19:54 +0200

    Fixing issue #4 and #10 + adding darkmode in settings tab with additional clarification
    Possibly solved issue #4 by remaking the ReplaceNode function.
    Solved issue #10 by using the bulk translating API, and replacing the text content per child node instead of the entire node and removing all child nodes.
    This also retains images/emojis.
    
    Added basic dark mode and additional clarifiaction in the settings if the local machine setting has it turned on.

--- inject.js
+++ inject.js
@@ -1,40 +1,73 @@
-// comments are displayed in either
-// 1) <yt-formatted-string id="content-text">"TEXT"</yt-formatted-string>
-// or
-// 2) <yt-formatted-string id="content-text"><span>"TEXTLINE1"</span><span></span><span>"TEXTLINE2"</span></yt-formatted-string>
-
 (function main () {
-	function ReplaceNode(a, b) {
-		a.parentNode.appendChild(b);
-		a.parentNode.removeChild(a);
+	function ReplaceNode(targetNode, newNode) {
+		// for some reason, using any replace method including the old method removes all children and adds the attribute "is-empty", creating a bug similarly mentioned in issue #4, so a workaround is to just replace all the text instead
+		// check if newnode has children
+		if (newNode.hasChildNodes()) {
+			for (let i = 0; i < targetNode.children.length; i++) {
+				targetNode.children[i].textContent = newNode.children[i].textContent;
+			}
+		}
+		else {
+			targetNode.textContent = newNode.textContent;
+		}
 	}
 
 	function TranslateButton_SetState() {
-		if (this._ntext.parentNode !== null) {
-			ReplaceNode(this._ntext, this._otext);
+		if (this._translated === true) {
+			ReplaceNode(this._cRef, this._otext);
 			this.innerText = TRANSLATE_TEXT;
 		} else {
-			ReplaceNode(this._otext, this._ntext);
+			ReplaceNode(this._cRef, this._ntext);
 			this.innerText = UNDO_TEXT;
 		}
+
+		this._translated = !this._translated;
 	}
 
 	function TranslateButton_Translate() {
 		this.onclick = TranslateButton_SetState;
-		fetch(`https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=${TARGET}&dt=t&q=${encodeURIComponent(this._otext.outerHTML)}`)
+		
+		if (this._otext.children.length === 0) {
+			fetch(`https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=${TARGET}&dt=t&q=${encodeURIComponent(this._otext.textContent)}`)
 			.then(response => response.json()).then(json => {
-				//for (let i = 0; i < json[0].length; i++) this._ntext.innerText += json[0][i][0].replace('\n', ' ');
-				let translatedHTML = "";
-				for (let i = 0; i < json[0].length; i++) baseTranslationString += json[0][i][0];
-				this._ntext.outerHTML = translatedHTML;
+				for (let i = 0; i < json[0].length; i++) this._ntext.textContent += json[0][i][0].replace('\n', ' ');
 				this.onclick();
 			});
+		}
+		else {
+			let text = [];
+			for (let i = 0; i < this._otext.children.length; i++) {
+				if (this._otext.children[i].textContent !== '' && this._otext.children[i].textContent !== '\n' && this._otext.children[i].textContent !== '\r') {
+					text.push(encodeURIComponent(this._otext.children[i].textContent));
+				}
+			}
+			fetch(`https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=${TARGET}&dt=t&q=${text.join('%0A')}`)
+			.then(response => response.json()).then(json => {
+				let j = 0;
+				for (let i = 0; i < this._otext.children.length; i++) {
+					if (this._ntext.children[i].textContent !== '' && this._ntext.children[i].textContent !== '\n' && this._ntext.children[i].textContent !== '\r') {
+						this._ntext.children[i].textContent = json[0][j][0].replace('\n', ' ');
+						j++;
+						if (j >= json[0].length) {
+							console.log("Out of bounds!");
+							console.log(json[0][j-1][0]);
+							console.log(this._ntext.children[i].textContent);
+
+							break;
+						}
+					}
+				}
+				this.onclick();
+			});
+		}
 	}
 
 	function ResetTranslateButton(tb) {
-		if (tb._ntext.parentNode !== null) ReplaceNode(tb._ntext, tb._otext);
+		if (tb._translated === true) ReplaceNode(tb._cRef, tb._otext);
 
-		tb._ntext.innerText = "";
+		//dispose of _ntext
+		tb._translated = false;
+		tb._ntext = tb._otext.cloneNode(true);
 		tb.innerText = TRANSLATE_TEXT;
 		tb.onclick = TranslateButton_Translate;
 	}
@@ -45,13 +78,15 @@
 		tb.style = "margin-left: 5px";
 		tb.classList = "yt-simple-endpoint style-scope yt-formatted-string";
 
-		tb._otext = main.querySelector(QS_CONTENT_TEXT);
-		tb._otext.addEventListener("DOMSubtreeModified", _ => ResetTranslateButton(tb));
+		tb._cRef = main.querySelector(QS_CONTENT_TEXT);
+		tb._otext = tb._cRef.cloneNode(true);
 
-		tb._ntext = document.createElement("div");
+
+		/*tb._ntext = document.createElement("div");
 		tb._ntext.style.whiteSpace = "pre-wrap";
 		tb._ntext.id = "content-text";
-		tb._ntext.classList = "style-scope ytd-comment-renderer translate-text yt-formatted-string";
+		tb._ntext.classList = "style-scope ytd-comment-renderer translate-text yt-formatted-string";*/
+
 
 		ResetTranslateButton(tb);
 		return tb;
@@ -59,6 +94,7 @@
 
 	/* Query Selectors */
 	// From main
+	const QS_BUTTON_FILTER = ".item.style-scope.yt-dropdown-menu";
 	const QS_TRANSLATE_BUTTON = "#header>#header-author>yt-formatted-string>#translate-button";
 	const QS_CONTENT_TEXT = "#expander>#content>#content-text";
 	const QS_BUTTON_CONTAINER = "#header>#header-author>yt-formatted-string";
@@ -85,7 +121,8 @@
 				/*if (mut.target.tagName.toLowerCase() == "ytd-comments") {
 					commentObserver.disconnect();
 					commentObserver.observe(mut.target, observerConfig);
-				} else */if (mut.target.id == "contents") {
+				} else */
+				if (mut.target.id == "contents") {
 					for (let n of mut.addedNodes) {
 						let main = n.querySelector("#body>#main");
 						if (!main) continue;
@@ -102,5 +139,16 @@
 		});
 
 		commentObserver.observe(document, observerConfig);
+
+		// reset the translate buttons when user changes the comment sorting filter
+		document.addEventListener("click", e => {
+			if (e.target.matches(QS_BUTTON_FILTER)) {
+				setTimeout(() => {
+					for (let n of document.querySelectorAll(QS_TRANSLATE_BUTTON)) {
+						ResetTranslateButton(n);
+					}
+				}, 100);
+			}
+		});
 	}
 })();
